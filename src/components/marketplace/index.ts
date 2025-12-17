1f274c3541942404f9789c6f2aee896694a05b6f
Vheek10 <jeremiahvictorgp@gmail.com>
2025-12-17 11:14:14 +0100

    removed marketfooter

--- src/components/marketplace/index.ts
+++ src/components/marketplace/index.ts
@@ -1,7 +1,8 @@
+/** @format */
+
 export { default as MarketplaceHeader } from "./MarketplaceHeader";
 export { default as FilterSidebar } from "./FilterSidebar";
 export { default as FilterSection } from "./FilterSection";
 export { default as PropertyCard } from "./PropertyCard";
 export { default as PropertyGrid } from "./PropertyGrid";
 export { default as ControlsBar } from "./ControlsBar";
-export { default as MarketplaceFooter } from "./MarketplaceFooter";
